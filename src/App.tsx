8e56e76924d9edc36cb8c5e10a295d1162b4695f
hon9n <mimmh93@gmail.com>
2023-10-06 23:22:45 +0900

    [feat] 헤더에 깃헙 아이콘 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react"
+import githubLogo from "./github-logo.svg"
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">마음 퀘스트</header>
+      <header className="App-header">
+        <div>마음 퀘스트</div>
+        <img className="Github-logo" src={githubLogo} />
+      </header>
       <img
         className="Test-main-img"
         src="https://user-images.githubusercontent.com/26381972/273206989-24da237c-8553-4edc-9916-0f8772dcdddd.png"
