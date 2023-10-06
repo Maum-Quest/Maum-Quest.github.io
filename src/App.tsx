f67fdd7b684dc6d928e1462f582da808e651adaf
hon9n <mimmh93@gmail.com>
2023-10-06 21:34:16 +0900

    [feat] 테스트 메인이미지 반응형이지만 이미지가 찌르러지지 않도록 함

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">마음 퀘스트</header>
-      <div className="Test-main-img">심리테스트 시작화면 이미지</div>
+      <img
+        className="Test-main-img"
+        src="https://user-images.githubusercontent.com/26381972/273206989-24da237c-8553-4edc-9916-0f8772dcdddd.png"
+      />
       <a className="Test-start-link" href="/" rel="noopener noreferrer">
         테스트 하러가기
       </a>
