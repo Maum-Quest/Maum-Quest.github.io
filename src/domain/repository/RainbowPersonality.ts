510d1eadf0f732b36edc6e66e929e23fef962205
hon9n <mimmh93@gmail.com>
2023-11-14 00:43:16 +0900

    [feat] 무지개 테스트 초안

---
+++ src/domain/repository/RainbowPersonality.ts
@@ -0,0 +1,97 @@
+const questions = [
+  {
+    question: "1번 질문 좀 길게 써봅시당 길게길게길게길게? 아주아주아주아주아주 길게 ~~~~~ 길게 ~~~~",
+    answers: [
+      {
+        content: "쿨쿨쿨",
+        type: ["Cool", "Dark"],
+        level: 1,
+      },
+      {
+        content: "이잉",
+        type: "Lovely",
+        level: 2,
+      },
+      {
+        content: "윽",
+        type: "Dark",
+        level: 2,
+      },
+      {
+        content: "쿨쿨쿨",
+        type: "Cool",
+        level: 1,
+      },
+    ],
+  },
+  {
+    question: "2번 질문 ?",
+    answers: [
+      {
+        content: "쿨쿨쿨",
+        type: ["Cool", "Dark"],
+        level: 1,
+      },
+      {
+        content: "이잉",
+        type: "Lovely",
+        level: 2,
+      },
+      {
+        content: "윽",
+        type: "Dark",
+        level: 2,
+      },
+      {
+        content: "쿨쿨쿨",
+        type: "Cool",
+        level: 1,
+      },
+    ],
+  },
+  {
+    question: "3번 질문 ?",
+    answers: [
+      {
+        content: "쿨쿨쿨",
+        type: ["Cool", "Dark"],
+        level: 1,
+      },
+      {
+        content: "이잉",
+        type: "Lovely",
+        level: 2,
+      },
+      {
+        content: "윽",
+        type: "Dark",
+        level: 2,
+      },
+      {
+        content: "쿨쿨쿨",
+        type: "Cool",
+        level: 1,
+      },
+    ],
+  },
+]
+
+const results = {
+  RED: "사랑이 넘치는 당신은 빨간색을 닮았어요.",
+  ORANGE: "호기심이 많은 당신은 주황색을 닮았어요.",
+  YELLOW: "밝고 에너지가 넘치는 당신은 노랑색을 닮았어요.",
+  GREEN: "침착하고 성실한 당신은 초록색을 닮았어요.",
+  BLUE: "시원시원한 당신은 파랑색을 닮았어요.",
+  INDIGO: "당신은 찬바람이 불어오는 밤하늘의 남색을 닮았어요.",
+  VIOLET: "오묘한 매력의 당신은 보라색을 닮았어요.",
+  BLACK: "어둠의 제왕이 오셨네요. 당신의 심장의 색깔은 BLACK 입니다.",
+}
+
+const data = {
+  questions: questions,
+  results: results,
+};
+
+export default data;
+
+
